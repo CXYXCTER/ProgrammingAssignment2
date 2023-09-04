255339db2e947ace2a6b43e28df489b74972a6de
SJZJCGe <2994674118@qq.com>
2023-09-05 00:49:56 +0800

    new commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,58 +1,62 @@
-## 生成一个新的开始
 ## Put comments here that give an overall description of what your
 ## functions do
-## 生成一个可逆矩阵
-n <- 3  
-B <- matrix(runif(n^2), nrow = n, ncol = n)  # 生成随机矩阵
-while (det(B) == 0) {
-        B <- matrix(runif(n^2), nrow = n, ncol = n)  # 重新生成矩阵直到行列式非零
-}
-print(B)
+
 ## Write a short comment describing this function
-## 储存inverse matrix
+## the comment is Chinese;translate if you'd like 
+## makeCacheMatrix会储存 矩阵和求逆的矩阵
+## create a matrix B
+# n <- 3  
+# B <- matrix(runif(n^2), nrow = n, ncol = n)  # 生成随机矩阵
+# while (det(B) == 0) {
+#         +         B <- matrix(runif(n^2), nrow = n, ncol = n)  # 重新生成矩阵直到行列式非零
+#         + }
+# print(B)
+
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+        m <- matrix()
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
         get <- function() x
-        setinv <- function(inv) m <<- solve(inv)
+        setinv <- function(inv) m <<- inv
         getinv <- function() m
-        catchobj <- list(set = set, get = get,
-             setinv = setinv,
-             getinv = getinv)
+        catchobj <- list(set = set, 
+                         get = get,
+                         setinv = setinv,
+                         getinv = getinv)
         return(catchobj)
 }
 
-
 ## Write a short comment describing this function
-## cacheSolve 1.计算inverse matrix 
-## 2.如果前面已经缓存，从缓存catch中获得inverse matricx
-
-
-
-cacheSolve <- function(x,makeCacheMatrix,...) {
-        ## Return a matrix that is the inverse of 'x'
-        m <- x$getinv()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
+## it seems that the  operator"$" is invalid in my R version(R-4.3);
+## and this work is very confusing.
+cacheSolve <- function(x, ...) {
+        s <- x$getinv
+        if(!is.null(s)) {
+                message("getting inversed matrix")
+                return(s)
         }
         data <- x$get()
-        m <- solve(data, ...)
-        x$setinv(m)
-        m
+        s <- solve(data, ...)
+        x$setinv(s)
+        s
 }
-myiver <- makeCacheMatrix(B)
 
-cacheSolve(A)
+cacheSolve(B)
+## Error in x$getinv : $ operator is invalid for atomic vectors
 
-A <- makeCacheMatrix()
 
-# 设置矩阵的值
-A$set(B)
 
-# 计算逆矩阵
-cacheSolve(A)
+
+
+
+
+
+
+
+
+
+
+
 
